12090f77de9f29f5b51415245576144081e887f3
Erik Doernenburg <erik@doernenburg.com>
2023-01-21 11:52:13 +0100

    Updated copyright banners and credits.

--- CCMenu/Source/Miscellaneous/WorkspaceController.swift
+++ CCMenu/Source/Miscellaneous/WorkspaceController.swift
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2007-2021 ThoughtWorks Inc.
+ *  Copyright (c) Erik Doernenburg and contributors
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
  *  not use these files except in compliance with the License.
  */
