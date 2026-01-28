1c13316bb345a2ec5bb9f6fc16b58bf2a2aa26e9
Erik Doernenburg <erik@doernenburg.com>
2026-01-28 15:40:14 +0100

    Added a couple of tests for the GitHubAPI functions.

--- CCMenuTests/GitHubAPITests.swift
+++ CCMenuTests/GitHubAPITests.swift
@@ -9,6 +9,27 @@ import XCTest
 
 class GitHubAPITests: XCTestCase {
 
+    func testConstructsRequestForFeed() throws {
+        let feed = PipelineFeed(type: .github, url: URL(string: "https://example.com/api")!)
+        let request = GitHubAPI.requestForFeed(feed: feed, token: nil)
+
+        guard let request else { XCTFail(); return }
+        XCTAssertEqual("GET", request.httpMethod)
+        XCTAssertEqual("application/vnd.github+json", request.value(forHTTPHeaderField: "Accept"))
+        XCTAssertEqual("2022-11-28", request.value(forHTTPHeaderField: "X-GitHub-Api-Version"))
+        let url = request.url?.absoluteString ?? ""
+        XCTAssertTrue(url.hasPrefix("https://example.com/api"))
+        XCTAssertTrue(url.contains("per_page="))
+    }
+
+    func testAddsAuthorizationHeaderWhenTokenIsGiven() throws {
+        let feed = PipelineFeed(type: .github, url: URL(string: "https://example.com/api")!)
+        let request = GitHubAPI.requestForFeed(feed: feed, token: "TEST-TOKEN")
+
+        guard let request else { XCTFail(); return }
+        XCTAssertEqual("Bearer TEST-TOKEN", request.value(forHTTPHeaderField: "Authorization"))
+    }
+
     func testConstructsCorrectRequestPathWhenBaseURLContainsPath() throws {
         UserDefaults.active.set("https://dev.some-enterprise.com/github", forKey: "GitHubAPIBaseURL")
         let request = GitHubAPI.requestForUser(user: "testuser", token: nil)
